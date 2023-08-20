dfd38b765b411c2ef11dc241aea467c5862c8f64
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-08-21 00:15:19 +0330

    Add hilt modules and setup database di

---
+++ app/src/main/java/com/androidisland/todocompose/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.androidisland.todocompose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.androidisland.todocompose.data.ToDoDao
+import com.androidisland.todocompose.data.ToDoDatabase
+import com.androidisland.todocompose.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideDatabase(context: Context): ToDoDatabase =
+        Room.databaseBuilder(
+            context,
+            ToDoDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+
+    @Singleton
+    @Provides
+    fun provideToDoDao(database: ToDoDatabase): ToDoDao = database.toDoDao()
+
+}
