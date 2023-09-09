ad3cd3b99ad851e0814ffae444b7807b9f09ba07
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-10 00:39:39 +0330

    Remove unnecessary annotations

--- app/src/main/java/com/androidisland/todocompose/MainActivity.kt
+++ app/src/main/java/com/androidisland/todocompose/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -39,7 +38,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToDoApp(
     navController: NavHostController,
