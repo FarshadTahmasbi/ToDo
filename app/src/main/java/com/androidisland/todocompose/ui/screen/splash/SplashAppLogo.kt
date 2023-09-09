e35ebb5013c84d23a2a99d54ba029fccf87cb8b2
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-10 02:59:26 +0330

    Implement splash screen and its logo

---
+++ app/src/main/java/com/androidisland/todocompose/ui/screen/splash/SplashAppLogo.kt
@@ -0,0 +1,92 @@
+package com.androidisland.todocompose.ui.screen.splash
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun SplashAppLogo(
+    logoSize: Dp
+) {
+    val backgroundColor = MaterialTheme.colorScheme.onPrimary
+    val textColor = MaterialTheme.colorScheme.primary
+
+    val textMeasurer = rememberTextMeasurer()
+    val textToDraw = "TO\nDO"
+    val textStyle = TextStyle(
+        fontSize = (logoSize.value / 4).sp,
+        fontWeight = FontWeight.W900,
+        fontFamily = FontFamily.Cursive,
+        color = textColor,
+    )
+    val textLayoutResult = remember(textToDraw) {
+        textMeasurer.measure(
+            textToDraw,
+            textStyle,
+        )
+    }
+
+    Canvas(
+        modifier = Modifier
+            .width(logoSize)
+            .aspectRatio(1f)
+    ) {
+        val triangleHeight = size.height * 1f / 3f
+        val squareSize = size.height * 2f / 3f
+        val paddingHorizontal = (size.width - squareSize) / 2f
+        val path = Path().apply {
+            moveTo(paddingHorizontal, 0f)
+            relativeLineTo(squareSize, 0f)
+            relativeLineTo(0f, squareSize)
+            relativeLineTo(-squareSize / 2f, triangleHeight)
+            relativeLineTo(-squareSize / 2f, -triangleHeight)
+            close()
+        }
+
+        drawIntoCanvas { canvas ->
+            canvas.drawOutline(
+                outline = Outline.Generic(path),
+                paint = Paint().apply {
+                    color = backgroundColor
+                    pathEffect = PathEffect.cornerPathEffect(size.maxDimension / 10f)
+                }
+            )
+        }
+
+        val textSize = textLayoutResult.size
+        drawText(
+            textLayoutResult = textLayoutResult,
+            topLeft = Offset(
+                x = size.width / 2f - textSize.width / 2f,
+                y = squareSize / 1.8f - textSize.height / 2f
+            ),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SplashAppLogoPreview() {
+    SplashAppLogo(logoSize = 120.dp)
+}
---
+++ app/src/main/java/com/androidisland/todocompose/ui/screen/splash/SplashScreen.kt
@@ -0,0 +1,31 @@
+package com.androidisland.todocompose.ui.screen.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        SplashAppLogo(logoSize = 160.dp)
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
+
