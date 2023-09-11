68961fcc71ad257faaee3825f76b9e44506a4ace
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-12 00:48:50 +0330

    Add delay and navigation to splash screen

--- app/src/main/java/com/androidisland/todocompose/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/androidisland/todocompose/ui/screen/splash/SplashScreen.kt
@@ -5,27 +5,34 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.androidisland.todocompose.util.Constants.SPLASH_DISPLAY_DURATION
+import com.androidisland.todocompose.util.Constants.SPLASH_LOGO_SIZE
+import kotlinx.coroutines.delay
 
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navigateToTaskList: () -> Unit) {
+    LaunchedEffect(key1 = Unit) {
+        delay(SPLASH_DISPLAY_DURATION)
+        navigateToTaskList()
+    }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.primary),
         contentAlignment = Alignment.Center
     ) {
-        SplashAppLogo(logoSize = 160.dp)
+        SplashLogo(logoSize = SPLASH_LOGO_SIZE)
     }
 }
 
 @Preview
 @Composable
 fun SplashScreenPreview() {
-    SplashScreen()
+    SplashScreen(navigateToTaskList = {})
 }
 
--- app/src/main/java/com/androidisland/todocompose/util/Constants.kt
+++ app/src/main/java/com/androidisland/todocompose/util/Constants.kt
@@ -1,9 +1,16 @@
 package com.androidisland.todocompose.util
 
+import androidx.compose.ui.unit.dp
+
 
 object Constants {
     const val DATABASE_TABLE = "todo_table"
     const val DATABASE_NAME = "todo_database"
 
     const val PREFERENCES_NAME = "settings"
+
+    const val SPLASH_DISPLAY_DURATION = 2000L
+
+    val SPLASH_LOGO_SIZE = 160.dp
+
 }
