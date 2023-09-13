19f243055a519ce0c8f0abfe9a4bdb163b7c64a4
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-13 20:48:15 +0330

    Add timestamp to ActionEvent to make same action and tasks distinct

--- app/src/main/java/com/androidisland/todocompose/util/ActionEvent.kt
+++ app/src/main/java/com/androidisland/todocompose/util/ActionEvent.kt
@@ -1,6 +1,12 @@
 package com.androidisland.todocompose.util
 
+import androidx.compose.runtime.Immutable
 import com.androidisland.todocompose.data.models.ToDoTask
 
 
-data class ActionEvent(val action: Action, val toDoTask: ToDoTask?)
+@Immutable
+data class ActionEvent(
+    val action: Action,
+    val toDoTask: ToDoTask?,
+    private val time: Long = System.currentTimeMillis()
+)
