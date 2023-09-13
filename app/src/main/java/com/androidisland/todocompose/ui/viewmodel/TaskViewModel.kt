018010680eab99ce3c50828a4c4fb1575b1911ed
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-14 01:20:09 +0330

    Remove unused channel from TaskViewModel

--- app/src/main/java/com/androidisland/todocompose/ui/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/androidisland/todocompose/ui/viewmodel/TaskViewModel.kt
@@ -1,16 +1,12 @@
 package com.androidisland.todocompose.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.androidisland.todocompose.util.ActionEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
-import kotlinx.coroutines.channels.Channel
 
 
 @HiltViewModel
-class TaskViewModel @Inject constructor(
-    private val actionEventChannel: Channel<ActionEvent?>
-) : ViewModel() {
+class TaskViewModel @Inject constructor() : ViewModel() {
     fun isValid(title: String, description: String): Boolean {
         return title.isNotEmpty() && description.isNotEmpty()
     }
