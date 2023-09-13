49684356a6402b3b76037154eb896aa97fc37edb
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-13 20:48:36 +0330

    Implement a swipe to dismiss composable using drag!

---
+++ app/src/main/java/com/androidisland/todocompose/ui/common/SwipeToDismiss.kt
@@ -0,0 +1,195 @@
+package com.androidisland.todocompose.ui.common
+
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
+enum class DismissDirection2 {
+    RightToLeft, LeftToRight;
+}
+
+sealed class DismissThreshold {
+    data class Percentage(val value: Float) : DismissThreshold()
+    data class Positional(val value: Dp) : DismissThreshold()
+}
+
+enum class DismissValue2 {
+    Default, DismissedToLeft, DismissedToRight
+}
+
+@Immutable
+data class DismissState2(
+    val isDismissed: Boolean,
+    val value: DismissValue2,
+    val progress: Float,
+    val isThresholdTouched: Boolean
+)
+
+private fun DismissThreshold.toPercentage(size: IntSize, density: Density) =
+    when (this) {
+        is DismissThreshold.Percentage -> {
+            value
+        }
+
+        is DismissThreshold.Positional -> {
+            with(density) {
+                value.toPx() / size.width
+            }
+        }
+    }
+
+private fun Float.signedProgressToDismissValue() =
+    when {
+        this < 0 -> DismissValue2.DismissedToLeft
+        this > 0 -> DismissValue2.DismissedToRight
+        else -> DismissValue2.Default
+    }
+
+@Composable
+fun SwipeToDismiss2(
+    directions: Set<DismissDirection2>,
+    dismissThreshold: (IntSize, Density) -> DismissThreshold = { size, density ->
+        DismissThreshold.Percentage(
+            0.2f
+        )
+    },
+    isEnabled: Boolean = true,
+    dismissDelay: Long = 200,
+    background: @Composable BoxScope.() -> Unit,
+    dismissContent: @Composable BoxScope.() -> Unit,
+    onDismissStateChange: (DismissState2) -> Unit
+) {
+
+    require(directions.isNotEmpty()) {
+        "Directions can not be empty"
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    val density = LocalDensity.current
+
+    val offsetX = remember { Animatable(0f) }
+    var size = remember { IntSize.Zero }
+    var signedProgress = remember { 0f }
+    var thresholdPercentage = remember { 0f }
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(IntrinsicSize.Max)
+        .onSizeChanged {
+            size = it
+            thresholdPercentage = dismissThreshold(it, density)
+                .toPercentage(it, density)
+        }) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Yellow), content = background
+        )
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .offset {
+                offsetX.value.let { offset ->
+                    val intOffset = if (offset < 0 && DismissDirection2.RightToLeft in directions) {
+                        IntOffset(offset.roundToInt(), 0)
+                    } else if (offset > 0 && DismissDirection2.LeftToRight in directions) {
+                        IntOffset(offset.roundToInt(), 0)
+                    } else {
+                        IntOffset.Zero
+                    }
+
+                    signedProgress = intOffset.x / size.width.toFloat()
+                    //Update value and direction state
+                    val dismissValue = signedProgress.signedProgressToDismissValue()
+
+                    //Invoke listener
+                    onDismissStateChange(
+                        DismissState2(
+                            isDismissed = false,
+                            value = dismissValue,
+                            progress = abs(signedProgress),
+                            isThresholdTouched = abs(signedProgress) >= thresholdPercentage
+                        )
+                    )
+
+                    intOffset
+                }
+            }
+            .draggable(state = rememberDraggableState { delta ->
+                coroutineScope.launch {
+                    offsetX.snapTo(offsetX.value + delta)
+                }
+            },
+                orientation = Orientation.Horizontal,
+                enabled = isEnabled,
+                reverseDirection = false,
+                onDragStopped = {
+                    coroutineScope.launch {
+                        //Not reached to the threshold, so we reset the drag state
+                        if (abs(signedProgress) < thresholdPercentage) {
+                            offsetX.animateTo(
+                                targetValue = 0f,
+                                animationSpec = tween(
+                                    durationMillis = 200, delayMillis = 0
+                                )
+                            )
+                        } else {
+                            val offset = when {
+                                signedProgress < 0 -> -size.width
+                                signedProgress > 0 -> size.width
+                                else -> 0
+                            }.toFloat()
+
+                            offsetX.animateTo(
+                                targetValue = offset,
+                                animationSpec = tween(
+                                    durationMillis = 200, delayMillis = 0
+                                )
+                            )
+
+                            delay(dismissDelay)
+                            val isSwipeDismissed = abs(signedProgress) >= thresholdPercentage
+                            if (isSwipeDismissed) {
+                                val dismissValue = signedProgress.signedProgressToDismissValue()
+                                //Invoke listener
+                                onDismissStateChange(
+                                    DismissState2(
+                                        isDismissed = true,
+                                        value = dismissValue,
+                                        progress = abs(signedProgress),
+                                        isThresholdTouched = abs(signedProgress) >= thresholdPercentage
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }), content = dismissContent
+        )
+    }
+}
--- app/src/main/java/com/androidisland/todocompose/ui/screen/list/ListContent.kt
+++ app/src/main/java/com/androidisland/todocompose/ui/screen/list/ListContent.kt
@@ -1,13 +1,8 @@
 package com.androidisland.todocompose.ui.screen.list
 
-import android.util.Log
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -17,14 +12,12 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -32,8 +25,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -43,17 +37,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import com.androidisland.todocompose.R
 import com.androidisland.todocompose.data.models.Priority
 import com.androidisland.todocompose.data.models.ToDoTask
 import com.androidisland.todocompose.ext.clickableThrottleFirst
+import com.androidisland.todocompose.ui.common.DismissDirection2
+import com.androidisland.todocompose.ui.common.DismissThreshold
+import com.androidisland.todocompose.ui.common.DismissValue2
+import com.androidisland.todocompose.ui.common.SwipeToDismiss2
 import com.androidisland.todocompose.ui.theme.HighPriorityColor
 import com.androidisland.todocompose.ui.theme.dimens
 import com.androidisland.todocompose.ui.viewmodel.SharedViewModel
 import com.androidisland.todocompose.util.Action
-import kotlinx.coroutines.launch
-import kotlin.math.roundToInt
 
 
 @Composable
@@ -79,7 +74,7 @@ fun ListContent(
             if (data.isEmpty()) {
                 EmptyContent()
             } else {
-                DisplayTasks(
+                ListSuccessContent(
                     tasks = data,
                     onSwipeDismiss = onSwipeDismiss,
                     navigateToTaskScreen = navigateToTaskScreen,
@@ -96,9 +91,8 @@ fun ListContent(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DisplayTasks(
+fun ListSuccessContent(
     tasks: List<ToDoTask>,
     onSwipeDismiss: (Action, ToDoTask) -> Unit,
     navigateToTaskScreen: (taskId: Int) -> Unit,
@@ -108,95 +102,36 @@ fun DisplayTasks(
         items(items = tasks, key = { task ->
             task.id
         }) { task ->
-            Log.d("test123", "progress->")
-
-//            val dismissState = rememberDismissState(
-//                confirmValueChange = {
-//                    Log.d("test123", "STATE CHANGE -> $it")
-//                    true
-//                },
-//                positionalThreshold = {
-//                    160.dp.toPx()
-//                }
-//            )
-//            val dismissDirection = dismissState.dismissDirection
-//            val isDismissed = dismissState.isDismissed(DismissDirection.EndToStart)
-//            Log.d("test123", "progress-> ${dismissState.progress}")
-//            if (isDismissed && dismissDirection == DismissDirection.EndToStart) {
-//                onSwipeDismiss(Action.DELETE, task)
-////                LaunchedEffect(key1 = Unit) {
-////                    scope.launch {
-////                        dismissState.reset()
-////                    }
-////                }
-//
-//            }
-//
-//            val degrees by animateFloatAsState(
-//                targetValue = if (dismissState.targetValue == DismissValue.Default) 0f else -45f,
-//                label = "Swipe Animation"
-//            )
-//            SwipeToDismiss(
-//                state = dismissState,
-//                background = {
-//                    SwipeBackground(degrees = degrees)
-//                },
-//                dismissContent = {
-//                    ToDoTaskItem(
-//                        toDoTask = task, navigateToTaskScreen = navigateToTaskScreen
-//                    )
-//                },
-//                directions = setOf(DismissDirection.EndToStart)
-//            )
-
-            DraggableItem(task = task, navigateToTaskScreen = navigateToTaskScreen)
-        }
-    }
-}
-
-@Composable
-fun DraggableItem(
-    task: ToDoTask,
-    navigateToTaskScreen: (taskId: Int) -> Unit
-) {
-//    val draggableState = rememberDraggableState(onDelta = {
-//        Log.d("test123", "drag delta -> $it")
-//    })
-    val coroutineScope = rememberCoroutineScope()
-    val offsetX = remember { Animatable(0f) }
-
-
-    Box(
-        modifier = Modifier
-            .offset {
-                IntOffset(offsetX.value.roundToInt(), 0)
+            var isSwiped: Boolean by remember {
+                mutableStateOf(false)
             }
-            .draggable(
-                state = rememberDraggableState { delta ->
-                    Log.d("test123", "DragDelta -> $delta, OffsetX->${offsetX.value}")
-                    coroutineScope.launch {
-                        offsetX.snapTo(offsetX.value + delta)
+
+            val degrees by animateFloatAsState(
+                targetValue = if (isSwiped.not()) 0f else -45f,
+                label = "Swipe Animation"
+            )
+
+            SwipeToDismiss2(
+                setOf(DismissDirection2.RightToLeft),
+                dismissThreshold = { size, density ->
+                    with(density) {
+                        DismissThreshold.Positional(size.height.toDp())
                     }
                 },
-                orientation = Orientation.Horizontal,
-                onDragStopped = {
-                    coroutineScope.launch {
-                        offsetX.animateTo(
-                            targetValue = 0f,
-                            animationSpec = tween(
-                                durationMillis = 300,
-                                delayMillis = 0
-                            ),
-//                            animationSpec = spring(dampingRatio = Spring.DampingRatioNoBouncy,
-//                                stiffness = Spring.StiffnessVeryLow,
-//                                visibilityThreshold = null),
-                            block = { Log.d("test123", "===>$it") }
-                        )
+                background = { SwipeBackground(degrees = degrees) },
+                dismissContent = {
+                    ToDoTaskItem(
+                        toDoTask = task,
+                        navigateToTaskScreen = navigateToTaskScreen
+                    )
+                },
+                onDismissStateChange = {
+                    isSwiped = it.isThresholdTouched
+                    if (it.isDismissed && it.value == DismissValue2.DismissedToLeft) {
+                        onSwipeDismiss(Action.DELETE, task)
                     }
-                }
-            )
-    ) {
-        ToDoTaskItem(toDoTask = task, navigateToTaskScreen = navigateToTaskScreen)
+                })
+        }
     }
 }
 
@@ -220,7 +155,8 @@ fun SwipeBackground(degrees: Float) {
 
 @Composable
 fun ToDoTaskItem(
-    toDoTask: ToDoTask, navigateToTaskScreen: (taskId: Int) -> Unit
+    toDoTask: ToDoTask,
+    navigateToTaskScreen: (taskId: Int) -> Unit
 ) {
     Surface(
         modifier = Modifier
@@ -258,7 +194,6 @@ fun ToDoTaskItem(
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 color = MaterialTheme.colorScheme.onSurface,
-//                textAlign = TextAlign.Justify
             )
         }
     }
