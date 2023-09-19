7da27d8c728127b0c80915dbe8f0a2c9d1859e3c
Farshad Tahmasbi <farshad.tmb@gmail.com>
2023-09-20 03:21:53 +0330

    Update Mvi contracts and its base ViewModel

--- app/src/main/java/com/androidisland/todocompose/arch/MviViewModel.kt
+++ app/src/main/java/com/androidisland/todocompose/arch/MviViewModel.kt
@@ -1,18 +1,22 @@
 package com.androidisland.todocompose.arch
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 
 
-abstract class MviViewModel<S : MviState, A : MviAction, E : MviEffect> : ViewModel() {
+abstract class MviViewModel<S : State, A : Action, E : Effect>(
+    protected val savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     abstract val initialState: S
 
@@ -22,35 +26,33 @@ abstract class MviViewModel<S : MviState, A : MviAction, E : MviEffect> : ViewMo
     private val _uiState: MutableStateFlow<S> by lazy { MutableStateFlow(initialState) }
     val state = _uiState.asStateFlow()
 
-    private val _action: MutableSharedFlow<A> = MutableSharedFlow()
-    val action = _action.asSharedFlow()
+    private val _action: MutableSharedFlow<A> = MutableSharedFlow(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
 
-    private val _effect: Channel<E> = Channel(Channel.BUFFERED)
-    val effect = _effect.receiveAsFlow()
+    private val _effect: Channel<E?> = Channel(Channel.BUFFERED)
+    val effect = _effect.receiveAsFlow().mapNotNull { it }
 
     init {
         collectActions()
     }
 
     private fun collectActions() {
-        action.onEach {
-            handleAction(it)
+        _action.onEach {
+            _uiState.value = reduce(currentState, it)
         }.launchIn(viewModelScope)
     }
 
-    fun applyAction(action: A) {
+    protected fun dispatchAction(action: A) {
         _action.tryEmit(action)
     }
 
-    protected abstract suspend fun handleAction(action: A)
+    protected abstract suspend fun reduce(currentState: S, action: A): S
 
-    protected fun setEffect(builder: () -> E) {
+    protected fun dispatchEffect(builder: () -> E) {
+        _effect.trySend(null)
         _effect.trySend(builder())
     }
-
-    protected fun setState(reducer: S.() -> S) {
-        val newState = currentState.reducer()
-        _uiState.value = newState
-    }
-
 }
